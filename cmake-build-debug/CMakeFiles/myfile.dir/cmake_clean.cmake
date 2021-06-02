a8224e97ad3c935d162e522fa8fe84486aae61b7
Ethan <esmoltz@gmail.com>
2021-06-02 16:20:37 -0400

    t

---
+++ cmake-build-debug/CMakeFiles/myfile.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myfile.dir/myfile.cpp.o"
+  "myfile"
+  "myfile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myfile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- myfile.cpp
+++ myfile.cpp
@@ -1,4 +1,37 @@
-//
-// Created by Ethan Shafran Moltz on 4/27/21.
-//
+#include <iostream>
+using namespace std;
 
+class LList;
+class PersonNode;
+typedef LList* LListPtr;
+typedef PersonNode* PersonNodePtr;
+
+class PersonNode{
+private:
+    double height;
+    double age;
+    double weight;
+    PersonNodePtr next;
+public:
+    PersonNode(double init_height = 0, double init_age = 0, double init_weight = 0) : height(init_height), age(init_age), weight(init_weight), next(nullptr){}
+    double get_height()const {return height;}
+    double get_age()const {return age;}
+    double get_weight() const {return weight;}
+
+    //setters
+    void set_next (PersonNodePtr next_node){next = next_node;}
+    friend class LList;
+
+    void operator + (const double num) {this->age += num;}
+
+};
+
+class LList{
+
+};
+
+
+int main() {
+
+    return 0;
+}
